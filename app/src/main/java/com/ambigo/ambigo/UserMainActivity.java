bd019e54961b615a610e85397af1bac306775df0
Prashanth K M <prasanthkm49@gmail.com>
2017-09-06 04:03:32 +0530

    Added Marker to User Current Location

--- app/src/main/java/com/ambigo/ambigo/UserMainActivity.java
+++ app/src/main/java/com/ambigo/ambigo/UserMainActivity.java
@@ -1,45 +1,63 @@
 package com.ambigo.ambigo;
 
 import android.app.Dialog;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class UserMainActivity extends AppCompatActivity implements OnMapReadyCallback {
+public class UserMainActivity extends AppCompatActivity implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
 
     GoogleMap mMap;
+    GoogleApiClient mGoogleApiClient;
+    LocationRequest mLocationRequest;
+    Marker marker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if(googleServicesAvailable()){
+        if (googleServicesAvailable()) {
             setContentView(R.layout.activity_user_main);
             initMap();
-        }else{
-
+        } else {
+            Toast.makeText(this, "play services not awailable, Can't Display Map", Toast.LENGTH_LONG).show();
         }
     }
 
-    private void initMap(){
+    private void initMap() {
         MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map_fragment);
         mapFragment.getMapAsync(this);
     }
-    public boolean googleServicesAvailable(){
+
+    public boolean googleServicesAvailable() {
         GoogleApiAvailability apiAvailability = GoogleApiAvailability.getInstance();
         int isAvailable = apiAvailability.isGooglePlayServicesAvailable(this);
-        if(isAvailable== ConnectionResult.SUCCESS){
+        if (isAvailable == ConnectionResult.SUCCESS) {
             return true;
-        }else if (apiAvailability.isUserResolvableError(isAvailable)){
-            Dialog dialog = apiAvailability.getErrorDialog(this, isAvailable,0);
+        } else if (apiAvailability.isUserResolvableError(isAvailable)) {
+            Dialog dialog = apiAvailability.getErrorDialog(this, isAvailable, 0);
             dialog.show();
-        }else {
-            Toast.makeText(this,"Can't Connect to play services", Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(this, "Can't Connect to play services", Toast.LENGTH_LONG).show();
         }
         return false;
     }
@@ -47,5 +65,60 @@ public class UserMainActivity extends AppCompatActivity implements OnMapReadyCal
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(LocationServices.API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        mLocationRequest = LocationRequest.create();
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        mLocationRequest.setInterval(1000);
+
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+            if(location == null){
+                Toast.makeText(this, "Can't Locate", Toast.LENGTH_LONG).show();
+            }else{
+                LatLng latLng = new LatLng(location.getLatitude(),location.getLongitude());
+                CameraUpdate update = CameraUpdateFactory.newLatLngZoom(latLng,16);
+
+                mMap.animateCamera(update);
+
+                if(marker != null){
+                    marker.remove();
+                }
+
+                MarkerOptions options = new MarkerOptions()
+                        .position(latLng);
+                marker=mMap.addMarker(options);
+            }
     }
 }
